b41991351fc518b983da4f2db29ad912837f59fc
decrypted-haider <haiderqadri12@gmail.com>
2026-08-30 20:54:15 +0500

    WOW

---
+++ 158A_NextRound.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() {
+    int n,k;
+    scanf("%d%d",&n,&k);
+    int pass=0;
+    int arr[n];
+    for(int i=0;i<n;i++) {
+        scanf("%d",&arr[i]);
+        if(arr[i] && arr[i]>=arr[k-1]) {
+            pass++;
+        }
+    }
+    printf("%d",pass);
+    return 0;
+}
