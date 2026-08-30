d9a46fcacb63d4f97b1be9e5d298c511a869b5d5
decrypted-haider <haiderqadri12@gmail.com>
2026-08-30 09:45:06 +0500

    Add: Trash to gitignore

---
+++ waytoolongword.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int strlength(char* s) {
+    int length=0;
+    while(s[length]!='\0') {
+        length++;
+    }
+    return length;
+}
+int main () {
+    int n,i,len;
+    scanf("%d",&n);
+    char s[n][101];
+    for(i=0;i<n;i++) {
+        scanf("%s",s[i]);
+    }
+    for(i=0;i<n;i++) {
+        len=strlength(s[i]);
+        if(len>10) {
+            printf("%c%d%c\n",s[i][0],len-2,s[i][len-1]);
+        } 
+        else {
+            printf("%s\n",s);
+        }
+    }    
+    return 0;
+}
