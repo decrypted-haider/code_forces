dad693753cf23fd729601c77bbbbbb1517caf553
decrypted-haider <haiderqadri12@gmail.com>
2026-09-12 21:43:35 +0500

    WOW

---
+++ 800/110A_NearlyLuckyNumber.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    int lucky=0;
+    while(n) {
+        if(n%10==4||n%10==7) {
+            lucky++;
+        }
+        n/=10;
+    }
+    if(lucky==4||lucky==7) {
+        printf("YES");
+    }
+    else {
+        printf("NO");
+    }
+    return 0;
+}
