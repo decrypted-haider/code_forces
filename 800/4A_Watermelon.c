992e78b19119e4520eab2aeea45b23c1c44945ea
decrypted-haider <haiderqadri12@gmail.com>
2026-09-13 17:40:24 +0500

    WOW

--- 800/4A_Watermelon.c
+++ 800/4A_Watermelon.c
@@ -2,11 +2,12 @@
 int main() {
     int w;
     scanf("%d",&w);
-    if(w/2%2==0) {
-        printf("Yes");
+    if(w>2&&w%2==0) {
+        if((w/2)%2==0) {
+            printf("YES");
+        }
+        else printf("NO");
     }
-    else {
-        printf("NO");
-    }
+    else printf("NO");
     return 0;
 }
