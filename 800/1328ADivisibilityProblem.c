d00f131df15f9ba515e523b84d620e73f5b82700
decrypted-haider <haiderqadri12@gmail.com>
2026-09-14 21:16:03 +0500

    WOW

--- 800/1328ADivisibilityProblem.c
+++ 800/1328ADivisibilityProblem.c
@@ -11,25 +11,25 @@ int main() {
         }
     }
     for(int i=0;i<t;i++) {
-        moves=0;
-        if(s[i][0]%s[i][1]==0) {
-            printf("%d\n",moves);
-            continue;
-        }
-        
-        else {   
-            while(s[i][0]%s[i][1]!=0) {
-            s[i][0]++;
-            moves++;
-            }
-        }
-        // hasnain solution:
-        // int curr = s[i][0]/s[i][1];
-        // if(curr*s[i][1]==s[i][0]) moves=0;
-        // else {
-        //     int diff = s[i][1]*(curr+1);
-        //     moves = diff - s[i][0];
+        // moves=0;
+        // if(s[i][0]%s[i][1]==0) {
+        //     printf("%d\n",moves);
+        //     continue;
         // }
+        
+        // else {   
+        //     while(s[i][0]%s[i][1]!=0) {
+        //     s[i][0]++;
+        //     moves++;
+        //     }
+        // }
+        // hasnain solution:
+        int curr = s[i][0]/s[i][1];
+        if(curr*s[i][1]==s[i][0]) moves=0;
+        else {
+            int diff = s[i][1]*(curr+1);
+            moves = diff - s[i][0];
+        }
         printf("%d\n",moves);
     }
     return 0;
