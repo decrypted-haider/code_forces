e2a717c4bc54770951560b74b15729673de9f536
decrypted-haider <haiderqadri12@gmail.com>
2026-09-16 20:08:30 +0500

    WOW

---
+++ 800/144A_ArrivalOfTheGeneral.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+int minimum(int arr[],int size) {
+    int minIdx=0;
+    for(int i=1;i<size;i++) {
+        if(arr[i]<arr[minIdx]) minIdx=i;
+    }
+    return minIdx;
+}
+int maximum(int arr[],int size) {
+    int maxIdx = 0;
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > arr[maxIdx]) maxIdx = i;
+    }
+    return maxIdx;
+}
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    int s[n];
+    for(int i=0;i<n;i++) {
+        scanf("%d",&s[i]);    
+    }
+    int moves=0;
+    int minIdx = minimum(s,n);
+    int maxIdx = maximum(s,n);
+    
+    if(minIdx<maxIdx) moves-=2;    
+    while(maxIdx!=0) {
+        maxIdx--;
+        moves++;
+        }
+    while(minIdx!=n-1) {
+        minIdx++;
+        moves++;
+    }
+    printf("%d",moves);
+    return 0;
+}
+
+
+
