34b402f429e5e62ad3d1a782523fca911e0bc864
decrypted-haider <haiderqadri12@gmail.com>
2026-09-18 17:59:00 +0500

    WOW

--- 800/996A_HitTheLottery.c
+++ 800/996A_HitTheLottery.c
@@ -1,21 +1,13 @@
 #include<stdio.h>
 int main() {
     int n;
-    int bills100=0;
-    int bills50=0;
-    int bills20=0;
-    int bills10=0;
-    int bills5=0;
-    int bills1=0;
-    int add=0;
+    int bills=0;
+    int denom[]={100,20,10,5,1};
     scanf("%d",&n);
-    bills100=n/100;
-    bills50=(n%100)/50;
-    bills20=((n%100)%50)/20;
-    bills10=(((n%100)%50)%20)/10;
-    bills5=((((n%100)%50)%20)%10)/5;
-    bills1=((((n%100)%50)%20)%10)%5;
-    add=bills100+bills50+bills20+bills10+bills5+bills1;
-    printf("%d",add);
+    for(int i=0;i<5;i++) {
+        bills+=n/denom[i];
+        n=n%denom[i];
+    }
+    printf("%d",bills);
     return 0;
 }
