000e7c34e0f6fed56a08799a8c8a06539de1e2e4
decrypted-haider <haiderqadri12@gmail.com>
2026-09-18 18:36:02 +0500

    WOW

---
+++ 800/443A_AntonAndLetters.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdbool.h>
+int main() {
+    char s[1000];
+    fgets(s,sizeof(s),stdin);
+    int letters=0;
+    int len=strlen(s);
+    bool freq[26]={false};
+    for(int i=1;i<=len;i+=3) {
+        freq[s[i]-'a']=true;
+    }
+    for(int i=0;i<26;i++) {
+        if(freq[i]) letters++;
+    }
+    printf("%d",letters);
+    return 0;
+}
